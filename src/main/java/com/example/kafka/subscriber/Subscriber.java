303eaf52ac66bbc9ae076336a159d6009d55478e
Azri <azri.akmal@elnusa.co.id>
2023-02-10 20:19:03 +0700

    first commit

---
+++ src/main/java/com/example/kafka/subscriber/Subscriber.java
@@ -0,0 +1,31 @@
+package com.example.kafka.subscriber;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class Subscriber {
+    @KafkaListener(topics = "hello", concurrency = "${spring.kafka.consumer.level.concurrency:3}")
+    public void subscribeHello(@Payload String message){
+        log.info("[hello] received message: " + message);
+    }
+
+    @KafkaListener(topics = "tea", concurrency = "${spring.kafka.consumer.level.concurrency:3}")
+    public void subscribeTea(@Payload String message){
+        log.info("[tea] received message: " + message);
+    }
+
+    @KafkaListener(topics = "coffee", concurrency = "${spring.kafka.consumer.level.concurrency:3}")
+    public void subscribeCoffee(@Payload String message){
+        log.info("[coffee] received message: " + message);
+    }
+
+    @KafkaListener(topics = "herb", concurrency = "${spring.kafka.consumer.level.concurrency:3}")
+    public void subscribeHerb(@Payload String message){
+        log.info("[herb] received message: " + message);
+    }
+
+}
